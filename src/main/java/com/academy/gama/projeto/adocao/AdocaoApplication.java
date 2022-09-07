c60ebbab6fde18536c88869a9c808b6e3a67a378
erick-ferraz <erickfpc.tech@gmail.com>
2022-09-07 19:36:20 -0300

    Initial commit

---
+++ src/main/java/com/academy/gama/projeto/adocao/AdocaoApplication.java
@@ -0,0 +1,13 @@
+package com.academy.gama.projeto.adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/gama/projeto/adocao/AdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.gama.projeto.adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
